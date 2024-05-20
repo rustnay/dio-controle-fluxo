afdbca1ae4461efe1be84f4ca3260357e13b6722
rustnay <cristianoalmeida.souza@gmail.com>
2024-05-20 17:41:56 -0300

    Add new feature to complete the control of flux class

---
+++ src/main/java/org/dio/controle_fluxo/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package org.dio.controle_fluxo.exceptions;
+
+public class ParametrosInvalidosException extends RuntimeException{
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
